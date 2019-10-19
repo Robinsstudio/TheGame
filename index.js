e21698343eea3850484b85b2c28f175e37064d9e
Robinsstudio <robin_1997@hotmail.fr>
2019-10-19 17:22:15 +0200

    Ajout cartes et joueurs au modèle

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/thegame', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Card = require('./model/Card');
+const Player = require('./model/Player');
