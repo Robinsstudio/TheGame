82ac6b3687353434cf9f8e9052e1a51f597209b4
Robinsstudio <robin_1997@hotmail.fr>
2019-10-20 16:52:03 +0200

    Utilisation de JSON web tokens pour l'authentification

--- model/Player.js
+++ model/Player.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 const util = require('util');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 const randomBytesAsync = util.promisify(crypto.randomBytes);
 const pbkdf2Async = util.promisify(crypto.pbkdf2);
+const signAsync = util.promisify(jwt.sign);
+
+const secretKey = fs.readFileSync(__dirname + '/../secret_key', 'utf8');
 
 const PlayerSchema = new mongoose.Schema({
 	login: String,
 	password: Buffer,
 	email: String,
 	registrationDate: Date,
-	salt: Buffer,
-	token: String
+	salt: Buffer
 });
 
 const NUM_ITERATIONS = 100000;
@@ -28,8 +32,8 @@ PlayerSchema.statics.register = function(login, password, email) {
 		return randomBytesAsync(256).then(function(buffer) {
 			return buffer;
 		}).then(function(salt) {
-			return pbkdf2Async(password, salt, NUM_ITERATIONS, HASH_SIZE, DIGEST).then(function(hashedPassword) {
-				return new Player({ login, password: hashedPassword, email, salt, registrationDate: Date.now() }).save();
+			return pbkdf2Async(password, salt, NUM_ITERATIONS, HASH_SIZE, DIGEST).then(function(hash) {
+				return new Player({ login, password: hash, email, salt, registrationDate: Date.now() }).save();
 			});
 		});
 	});
@@ -43,10 +47,11 @@ PlayerSchema.statics.authenticate = function(login, password) {
 		}
 
 		const player = players[0];
-		return pbkdf2Async(password, player.salt, NUM_ITERATIONS, HASH_SIZE, DIGEST).then(function(hashedPassword) {
-			if (Buffer.compare(hashedPassword, player.password)) {
+		return pbkdf2Async(password, player.salt, NUM_ITERATIONS, HASH_SIZE, DIGEST).then(function(hash) {
+			if (Buffer.compare(hash, player.password)) {
 				throw new Error('Incorrect password');
 			}
+			return signAsync({ idPlayer: player._id }, secretKey);
 		});
 	});
 }
