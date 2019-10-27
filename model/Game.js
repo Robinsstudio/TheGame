11d6ba2a08fbd1c486e0d162d9777aa86adbef7b
Baptiste <cormeray@live.fr>
2019-10-27 16:24:07 +0100

    Ajout des fonctions pour jouer une carte et passer le tour

--- model/Game.js
+++ model/Game.js
@@ -1,21 +1,13 @@
 const mongoose = require('mongoose');
 const Card = require('./Card');
-const PileSchema = new mongoose.Schema({
-	orientation : {
-	  type : String,
-	  enum : [ 'up' , 'down' ],
-	  default : 'up'
-	},
-	cards : [ Card.schema ]
-});
-const Pile = mongoose.model('Pile',PileSchema);
+const Pile = require('./Pile');
 const GameSchema = new mongoose.Schema({
 	players : [ {
 		_id : String,
 		hand : [ Card.schema ]
 	} ],
 	deckPile : [ Card.schema ],
-	piles : [ PileSchema ],
+	piles : [ Pile.schema ],
 	nowPlaying : String, //Le joueur du tour
 	actions : [ 
 			{
@@ -35,22 +27,23 @@ const GameSchema = new mongoose.Schema({
 	}
 	
 });
+
+let shuffle = function (a) {
+	for (let i = a.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[a[i], a[j]] = [a[j], a[i]];
+	}
+	return a;
+};
+
 //Création d'une nouvelle partie
 GameSchema.statics.createGame = function(){
-	let shuffle = function (a) {
-		for (let i = a.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			[a[i], a[j]] = [a[j], a[i]];
-		}
-		return a;
-	};
 	return Card.find(/*{value:{$gte: 2, $lte : 99}}*/)
 	.then(function(result){
 		return new Game({
 			deckPile:shuffle(result),
 			piles : [ new Pile({orientation:'down'}),new Pile({orientation : 'down'}),new Pile({}),new Pile({}) ]
-		}).save()
-		.then(res=>{console.log(res._id);return res;});
+		}).save();
 	});
 }
 
@@ -67,13 +60,21 @@ GameSchema.statics.drawCard = function(game){
 	return game.deckPile.pop();
 }
 
-//Rempli la main d'un joueur jusqu'à ce que celui ci ait 5 cartes
+//Remplit la main d'un joueur jusqu'à ce que celui ci ait 5 cartes
 GameSchema.statics.refillPlayerHand = function(gameId, playerId){
 	return Game.findOne({_id:gameId, 'players._id': playerId})
 	.then(res=>{
 		console.log(res);
 		if(res !== null && res.players !== null){
-			res.players.filter((ele)=>ele._id===playerId && ele.hand.length<5).map(ele=>{
+			res.players.filter((ele)=>ele._id==playerId && ele.hand.length<5).map(ele=>{
+				/*
+				let card = "";
+				while(card !== null && ele.hand.length<5){
+					card = Game.drawCard(res);
+					if(card !== null)
+						ele.hand.push(card);
+				}
+				*/
 				let card;
 				do{
 					card = Game.drawCard(res);
@@ -88,16 +89,109 @@ GameSchema.statics.refillPlayerHand = function(gameId, playerId){
 	});
 }
 
+GameSchema.statics.getPlayer = function(game,playerId){
+	let player = game.players.filter(ele=>ele._id === playerId)[0];
+	if(player === undefined){
+		throw Error("Le joueur n'existe pas");
+	}
+	return player;
+}
 
+GameSchema.statics.getCardFromPlayerHand = function(game,playerId,cardId){
+	let player = Game.getPlayer(game,playerId);
+	let card = player.hand.filter(ele=>ele._id==cardId)[0];
+	if(card === undefined){
+		throw Error("La carte n'est pas présente dans le jeu du joueur");
+	}
+	return card;
+}
+
+GameSchema.statics.removeCardFromPlayerHand = function(game,playerId,cardId){
+	let player = Game.getPlayer(game,playerId);
+	player.hand = player.hand.filter(ele=>ele._id!==cardId);
+}
+
+GameSchema.statics.putCardOnPile = function(game,playerId,pile,card){
+	if(pile.cards.length===0){
+		Game.removeCardFromPlayerHand(game,playerId,card._id);
+		pile.cards.push(card);
+	}
+	else{
+		if(pile.orientation === 'down' && pile.cards[pile.cards.length-1].value > card.value){
+			Game.removeCardFromPlayerHand(game,playerId,card._id);
+			pile.cards.push(card);
+		}
+		else if(pile.orientation === 'up' && pile.cards[pile.cards.length-1].value < card.value){
+			Game.removeCardFromPlayerHand(game,playerId,card._id);
+			pile.cards.push(card);
+		}
+		else{
+			throw Error("Impossible de poser cette carte sur la pile");
+		}
+	}
+}
 //Jouer une carte
-GameSchema.statics.playCard = function(gameId,playerId,cardId){
-	return new Promise(function(resolve,reject){
-		Game.findOne({_id : gameId, "players._id" : playerId})
-		.then(result => {
-			
-		})
-		.catch(err=>{console.log(err);reject("Erreur lors du lancement de la fonction playCard")})
+GameSchema.statics.playCard = function(gameId,playerId,cardId,pileId){
+	return Game.findOne({_id : gameId})
+	.then(game => {
+		if(game!==null){
+			if(game.status!=="playing"){
+				throw Error("Cette action ne peut pas être réalisée actuellement");
+			}
+			if(game.nowPlaying !== playerId){
+				throw Error("Ce n'est pas le tour de "+playerId);
+			}
+			let pile = game.piles.filter(ele=>ele._id==pileId)[0];
+			if (pile===undefined){
+				throw Error("La pile n'existe pas");
+			}
+			else{
+				let card = Game.getCardFromPlayerHand(game,playerId,cardId);
+				Game.putCardOnPile(game,playerId,pile,card);
+			}
+			return game.save();
+		}
+		else{
+			throw Error("Partie introuvable");
+		}
 	});
+}
+
+GameSchema.statics.getNextPlayer = function(game){
+	let playerPos = game.players.map(ele=>ele._id.toString()).indexOf(game.nowPlaying)+1;
+	if(playerPos===game.players.length)
+		playerPos=0;
+	return game.players[playerPos]._id;
+}
+
+
+//finir son tour
+GameSchema.statics.endTurn = function(gameId,playerId){
+	return Game.findOne({_id:gameId})
+	.then(game=>{
+		if(game.status!=="playing"){
+			throw Error("Cette action ne peut pas être réalisée actuellement");
+		}
+		if(game.nowPlaying !== playerId){
+			throw Error("Ce n'est pas le tour de "+playerId);
+		}
+		let player = Game.getPlayer(game,playerId);
+		if(player.hand.length>3 && game.deckPile.length>0){
+			throw Error("Vous n'avez pas joué assez de cartes");
+		}
+		else if(player.hand.length>4 && game.deckPile.length===0){
+			throw Error("Vous n'avez pas joué assez de cartes");
+		}
+		game.nowPlaying=Game.getNextPlayer(game);
+		Game.refillPlayerHand(gameId,playerId);
+		return game.save();
+	});
+}
+
+
+//récupérer les actions précédentes
+GameSchema.statics.getActions = function(gameId,playerId,version){
+	return ;
 }
 
 const Game = mongoose.model('Game', GameSchema);
---
+++ model/Pile.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Card = require('./Card');
+const PileSchema = new mongoose.Schema({
+	orientation : {
+	  type : String,
+	  enum : [ 'up' , 'down' ],
+	  default : 'up'
+	},
+	cards : [ Card.schema ]
+});
+const Pile = mongoose.model('Pile',PileSchema);
+
+module.exports = Pile;
