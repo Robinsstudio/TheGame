1105b8a0a8c5a6fde496ac2ae0b2b7a96194d452
Robinsstudio <robin_1997@hotmail.fr>
2020-01-20 16:37:43 +0100

    Update cards.js

--- front_thegame/src/components/LobbyPage/cards.js
+++ front_thegame/src/components/LobbyPage/cards.js
@@ -140,9 +140,6 @@ var cards = (function() {
       ypos = -offsets[this.suit] * opt.cardSize.height;
       this.rotate(0);
       if (rank >= 0) {
-        if (rank === 1 && rank === 100) {
-          $(this.el).removeClass("Visible");
-        }
         // Si la carte est dans la main du joueur alors on ajoute la classe Visible pour qu'elle grossisse
         if (this.container instanceof Hand) {
           $(this.el).addClass("Visible");
